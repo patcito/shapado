91a89e0d35ea64e9131b04f4408d269f219ca454
David A. Cuadrado <krawek@gmail.com>
2009-07-05 19:36:10 -0500

    users, authentication
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

---
+++ app/models/user.rb
@@ -0,0 +1,64 @@
+require 'digest/sha1'
+
+class User
+  include CouchMapper::Document
+
+  include Authentication
+  include Authentication::ByPassword
+  include Authentication::ByCookieToken
+
+  key :login,                     String, :limit => 40
+  key :name,                      String, :limit => 100, :default => '', :null => true
+  key :email,                     String, :limit => 100
+  key :crypted_password,          String, :limit => 40
+  key :salt,                      String, :limit => 40
+  key :created_at,                Time
+  key :updated_at,                Time
+  key :remember_token,            String, :limit => 40
+  key :remember_token_expires_at, Time
+
+
+  validates_presence_of     :login
+  validates_length_of       :login,    :within => 3..40
+  validates_uniqueness_of   :login
+  validates_format_of       :login,    :with => Authentication.login_regex, :message => Authentication.bad_login_message
+
+  validates_format_of       :name,     :with => Authentication.name_regex,  :message => Authentication.bad_name_message, :allow_nil => true
+  validates_length_of       :name,     :maximum => 100
+
+  validates_presence_of     :email
+  validates_length_of       :email,    :within => 6..100 #r@a.wk
+  validates_uniqueness_of   :email
+  validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
+
+
+
+  # HACK HACK HACK -- how to do attr_accessible from here?
+  # prevents a user from submitting a crafted form that bypasses activation
+  # anything else you want your user to change should be added here.
+  attr_accessible :login, :email, :name, :password, :password_confirmation
+
+
+
+  # Authenticates a user by their login name and unencrypted password.  Returns the user or nil.
+  #
+  # uff.  this is really an authorization, not authentication routine.
+  # We really need a Dispatch Chain here or something.
+  # This will also let us return a human error message.
+  #
+  def self.authenticate(login, password)
+    return nil if login.blank? || password.blank?
+    u = find(:first, :conditions => {:login => login.downcase}) # need to get the salt
+    u && u.authenticated?(password) ? u : nil
+  end
+
+  def login=(value)
+    write_attribute :login, (value ? value.downcase : nil)
+  end
+
+  def email=(value)
+    write_attribute :email, (value ? value.downcase : nil)
+  end
+
+  protected
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.resources :users
+
+  map.resource :session
+
   map.resources :questions
 
   # The priority is based upon order of creation: first created -> highest priority.
