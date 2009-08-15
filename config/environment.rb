b6aaf67f31ddef1da297d3b416a807009741d6b5
David A. Cuadrado <krawek@gmail.com>
2009-08-14 23:05:19 -0500

    add deps
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "mongomapper"
+  config.gem "chriseppstein-compass", :lib => "compass"
+  config.gem "chriseppstein-compass-960-plugin", :lib => "ninesixty"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
