22ef81eda32d431407cbcba7acc1a77e37044dce
Jorge Cuadrado <kuadrosxx@gmail.com>
2009-08-14 22:36:21 -0500

    added compass and implemented a basic layout
    Signed-off-by: Jorge Cuadrado <kuadrosxx@gmail.com>

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -28,6 +28,7 @@ Rails::Initializer.run do |config|
   # Skip frameworks you're not going to use. To use Rails without a database,
   # you must remove the Active Record framework.
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
+  config.frameworks -= [ :active_record]
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
@@ -41,4 +42,4 @@ Rails::Initializer.run do |config|
   # config.i18n.default_locale = :de
 end
 
-MongoMapper.database = "shapado" # FIXME
+MongoMapper.database = "shapado-#{Rails.env}" # FIXME
