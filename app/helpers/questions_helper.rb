75528e293ede8d07cc4aa2895593f9ca53692d91
Jorge Cuadrado <kuadrosxx@gmail.com>
2009-08-15 02:45:38 -0500

    Implements vote_box helper
    Signed-off-by: Jorge Cuadrado <kuadrosxx@gmail.com>

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def vote_box(average = 0)
+    "<span class='votes'>
+      #{link_to "↑", "", :class=> "vote_up"}
+      #{average}
+      #{link_to "↓", "", :class=>"vote_down"}
+    </span>"
+  end
 end
