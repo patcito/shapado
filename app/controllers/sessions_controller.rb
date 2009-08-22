f462ac7c90a869e92e006959b22132a24ba78a16
David A. Cuadrado <krawek@gmail.com>
2009-08-22 01:09:31 -0500

    skip authenticity token for sessions/create
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 # This controller handles the login/logout function of the site.
 class SessionsController < ApplicationController
+  skip_before_filter :verify_authenticity_token, :only => [:create]
+
   include OpenIdAuthentication
 
   # render new.rhtml
