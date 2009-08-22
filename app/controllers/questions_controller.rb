6f5d174deb3059fcfe727d9f9df3f7d84c60e542
David A. Cuadrado <krawek@gmail.com>
2009-08-22 00:45:45 -0500

    put openid login in the front page
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_filter :login_required, :except => [:index, :show]
+  before_filter :login_required, :except => [:index, :show, :tags, :unanswered]
   before_filter :set_active_tag
 
   tabs :default => :questions, :tags => :tags,
