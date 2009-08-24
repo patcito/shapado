fc63215e0ec7c01db8e54b418ef32f05112b694a
David A. Cuadrado <krawek@gmail.com>
2009-08-24 00:09:14 -0500

    some JS fixes
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -21,83 +21,83 @@ function setupEditor() {
       this.wrapSelection('*','*');
   },{
     id: 'markdown_italics_button'
-  });  
+  });
 
-  toolbar.addButton('Bold',function(){  
-      this.wrapSelection('**','**');  
-  },{  
-    id: 'markdown_bold_button'  
-  });  
+  toolbar.addButton('Bold',function(){
+      this.wrapSelection('**','**');
+  },{
+    id: 'markdown_bold_button'
+  });
 
-  toolbar.addButton('Link',function(){  
-    var selection = this.getSelection();  
-    var response = prompt('Enter Link URL','');  
-    if(response == null)  
-        return;  
+  toolbar.addButton('Link',function(){
+    var selection = this.getSelection();
+    var response = prompt('Enter Link URL','');
+    if(response == null)
+        return;
     this.replaceSelection('[' + (selection == '' ? 'Link Text' : selection) + '](' + (response == '' ? 'http://link_url/' : response).replace(/^(?!(f|ht)tps?:\/\/)/,'http://') + ')');
   },{
-    id: 'markdown_link_button'  
-  });  
+    id: 'markdown_link_button'
+  });
 
-  toolbar.addButton('Image',function(){  
+  toolbar.addButton('Image',function(){
     var selection = this.getSelection();
-    var response = prompt('Enter Image URL','');  
-    if(response == null)  
-        return;  
-    this.replaceSelection('![' + (selection == '' ? 'Image Alt Text' : selection) + '](' + (response == '' ? 'http://image_url/' : response).replace(/^(?!(f|ht)tps?:\/\/)/,'http://') + ')');  
-  },{  
-    id: 'markdown_image_button'  
-  });  
+    var response = prompt('Enter Image URL','');
+    if(response == null)
+        return;
+    this.replaceSelection('![' + (selection == '' ? 'Image Alt Text' : selection) + '](' + (response == '' ? 'http://image_url/' : response).replace(/^(?!(f|ht)tps?:\/\/)/,'http://') + ')');
+  },{
+    id: 'markdown_image_button'
+  });
 
-  toolbar.addButton('Heading',function(){  
-    var selection = this.getSelection();  
-    if(selection == '')  
-        selection = 'Heading';  
-    this.replaceSelection("\n" + selection + "\n" + $R(0,Math.max(5,selection.length)).collect(function(){'-'}).join('') + "\n");  
-  },{  
-    id: 'markdown_heading_button'  
-  });  
+  toolbar.addButton('Heading',function(){
+    var selection = this.getSelection();
+    if(selection == '')
+        selection = 'Heading';
+    this.replaceSelection('##'+selection+'##');
+  },{
+    id: 'markdown_heading_button'
+  });
 
-  toolbar.addButton('Unordered List',function(event){  
-    this.collectFromEachSelectedLine(function(line){  
-        return event.shiftKey ? (line.match(/^\*{2,}/) ? line.replace(/^\*/,'') : line.replace(/^\*\s/,'')) : (line.match(/\*+\s/) ? '*' : '* ') + line;  
-    });  
-  },{  
-    id: 'markdown_unordered_list_button'  
-  });  
+  toolbar.addButton('Unordered List',function(event){
+    this.collectFromEachSelectedLine(function(line){
+        return event.shiftKey ? (line.match(/^\*{2,}/) ? line.replace(/^\*/,'') : line.replace(/^\*\s/,'')) : (line.match(/\*+\s/) ? '*' : '* ') + line;
+    });
+  },{
+    id: 'markdown_unordered_list_button'
+  });
 
-  toolbar.addButton('Ordered List',function(event){  
-    var i = 0;  
-    this.collectFromEachSelectedLine(function(line){  
-        if(!line.match(/^\s+$/)){  
-            ++i;  
-            return event.shiftKey ? line.replace(/^\d+\.\s/,'') : (line.match(/\d+\.\s/) ? '' : i + '. ') + line;  
-        }  
-    });  
-  },{  
-    id: 'markdown_ordered_list_button'  
-  });  
+  toolbar.addButton('Ordered List',function(event){
+    var i = 0;
+    this.collectFromEachSelectedLine(function(line){
+        if(!line.match(/^\s+$/)){
+            ++i;
+            return event.shiftKey ? line.replace(/^\d+\.\s/,'') : (line.match(/\d+\.\s/) ? '' : i + '. ') + line;
+        }
+    });
+  },{
+    id: 'markdown_ordered_list_button'
+  });
 
-  toolbar.addButton('Block Quote',function(event){  
-    this.collectFromEachSelectedLine(function(line){  
-        return event.shiftKey ? line.replace(/^\> /,'') : '> ' + line;  
-    });  
-  },{  
-    id: 'markdown_quote_button'  
-  });  
+  toolbar.addButton('Block Quote',function(event){
+    this.collectFromEachSelectedLine(function(line){
+        return event.shiftKey ? line.replace(/^\> /,'') : '> ' + line;
+    });
+  },{
+    id: 'markdown_quote_button'
+  });
 
-  toolbar.addButton('Code Block',function(event){  
-    this.collectFromEachSelectedLine(function(line){  
-        return event.shiftKey ? line.replace(/    /,'') : '    ' + line;  
-    });  
-  },{  
-    id: 'markdown_code_button'  
-  });  
+  toolbar.addButton('Code Block',function(event){
+    this.collectFromEachSelectedLine(function(line){
+        return event.shiftKey ? line.replace(/    /,'') : '    ' + line;
+    });
+  },{
+    id: 'markdown_code_button'
+  });
 
-  toolbar.addButton('Help',function(){  
-    window.open('http://daringfireball.net/projects/markdown/dingus');  
-  },{  
-    id: 'markdown_help_button'  
+  toolbar.addButton('Help',function(){
+    window.open('http://daringfireball.net/projects/markdown/dingus');
+  },{
+    id: 'markdown_help_button'
   });
 }
 
--- public/javascripts/modules/sessions.js
+++ public/javascripts/modules/sessions.js
@@ -1,10 +1,16 @@
 $(function() {
+ var oid = $('#openid');
+
+ if(oid.length == 0)
+   return;
+
  $('#openid').openid({
-  txt: {
-  label: "Enter your <b>{provider}</b> {username}",
-  username: 'login',
-  title: 'Select your account provider.',
-  sign: 'Sign-in'
- }
+   txt: {
+     label: "Enter your <b>{provider}</b> {username}",
+     username: 'login',
+     title: 'Select your account provider.',
+     sign: 'Sign-in'
+   }
  });
 });
+
