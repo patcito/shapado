b0bfe7341e29899d5624ec974fe5c4c1936595b1
David A. Cuadrado <krawek@gmail.com>
2009-08-25 03:43:21 -0500

    Add arrows for votes
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,9 +1,11 @@
 module QuestionsHelper
   def vote_box(average = 0)
-    "<span class='votes'>
-      #{link_to "↑", "", :class=> "vote_up"}
-      #{average}
-      #{link_to "↓", "", :class=>"vote_down"}
-    </span>"
+    %@<span class='votes'>
+      #{link_to image_tag("vote_up.png", :width => 34, :height => 22), "", :class=> "vote_up", :style => "display:block", :title => "Vote Up"}
+      <span style="display:block">
+        #{average}
+      </span>
+      #{link_to image_tag("vote_down.png", :width => 30, :height => 22), "", :class=>"vote_down", :style => "display:block", :title => "Vote Down"}
+    </span>@
   end
 end
