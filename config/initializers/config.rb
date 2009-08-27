cd62ed7a2618fba5151a3abede17b016f41f3e97
David A. Cuadrado <krawek@gmail.com>
2009-08-26 23:09:14 -0500

    added app config; make cookies work across subdomains
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

---
+++ config/initializers/config.rb
@@ -0,0 +1,10 @@
+require 'ostruct'
+
+config_file = RAILS_ROOT+"/config/shapado.yml"
+options = YAML.load_file(config_file)
+if !options[RAILS_ENV]
+  raise "#{RAILS_ENV} was not found in #{config_file}"
+end
+AppConfig = OpenStruct.new(options[RAILS_ENV])
+
+ActionController::Base.session_options[:domain] = ".#{AppConfig.domain}"
