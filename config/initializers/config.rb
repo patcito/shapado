71c4e21afe93314273e2f04ec454dfa2ad37e42f
David A. Cuadrado <krawek@gmail.com>
2009-08-28 02:22:39 -0500

    Fix config initializer
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -5,7 +5,6 @@ options = YAML.load_file(config_file)
 if !options[RAILS_ENV]
   raise "#{RAILS_ENV} was not found in #{config_file}"
 end
-AppConfig = OpenStruct.new(options)
-AppConfig.domain = options[RAILS_ENV]["domain"]
-[:development, :production, :test].each {|f| AppConfig.delete_field(f)}
+AppConfig = OpenStruct.new(options[RAILS_ENV])
 ActionController::Base.session_options[:domain] = ".#{AppConfig.domain}"
+
