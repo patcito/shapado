244dc4ed45839622a4f45a9cd65ddfd94425bb93
David A. Cuadrado <krawek@gmail.com>
2009-08-29 02:40:00 -0500

    use /etc/shapado.yml as config file if available
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,10 +1,15 @@
 require 'ostruct'
 
-config_file = RAILS_ROOT+"/config/shapado.yml"
+config_file = "/etc/shapado.yml"
+if !File.exist?(config_file)
+  config_file = RAILS_ROOT+"/config/shapado.yml"
+end
+
 options = YAML.load_file(config_file)
 if !options[RAILS_ENV]
-  raise "#{RAILS_ENV} was not found in #{config_file}"
+  raise "'#{RAILS_ENV}' was not found in #{config_file}"
 end
+
 AppConfig = OpenStruct.new(options[RAILS_ENV])
 ActionController::Base.session_options[:domain] = ".#{AppConfig.domain}"
 
