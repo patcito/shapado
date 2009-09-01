40de8af559b057c5091247183e47765552a3b87d
David A. Cuadrado <krawek@gmail.com>
2009-09-01 01:49:27 -0500

    minor fixes
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_login_or_id(params[:id])
     @questions = @user.questions.paginate(:page=>params[:questions_page], :per_page => 10)
-    @answers = @user.answers.paginate(:page=>params[:answers_page], :per_page => 10)
+    @answers = @user.answers.paginate(:page=>params[:answers_page], :conditions => {:parent_id => nil}, :per_page => 10)
   end
 
   def edit
