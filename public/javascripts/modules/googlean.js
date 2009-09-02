bd57d0ccd4c44cb3141629e1a912c81c0ba8215b
Patrick Aljord <patcito@gmail.com>
2009-09-02 03:18:34 -0500

    added id to googlean link

--- public/javascripts/modules/googlean.js
+++ public/javascripts/modules/googlean.js
@@ -2,7 +2,7 @@ var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "htt
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 
 try {
-  var code = $("a.googlean").attr("href");
+  var code = $("a#googlean").attr("href");
   var pageTracker = _gat._getTracker(code);
   pageTracker._trackPageview();
 } catch(err) {}
