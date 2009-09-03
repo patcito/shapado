52ee9dc36fe412b772f5c0fc0324962dee053f7a
David A. Cuadrado <krawek@gmail.com>
2009-09-03 03:32:02 -0500

    small fix
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -15,7 +15,8 @@ class AnswersController < ApplicationController
     if @question && @answer.save
       unless @answer.comment?
         @question.answer_added!
-        if @question.user.blank? && @question.user.notification_opts["new_answer"] == "1"
+        email = @question.user.email
+        if !email.blank? && @question.user.notification_opts["new_answer"] == "1"
           Notifier.deliver_new_answer(@question.user, @answer)
         end
         current_user.update_reputation(:answer_question)
