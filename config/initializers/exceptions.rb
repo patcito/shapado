ac1b96d35d36f35d3969d15dce920ea4ebf6a2d0
Jorge Cuadrado <kuadrosxx@gmail.com>
2009-09-15 16:24:32 -0500

    allow configuring sender address by using shapado.yml for exception notifier
    Signed-off-by: Jorge Cuadrado <kuadrosxx@gmail.com>

--- config/initializers/exceptions.rb
+++ config/initializers/exceptions.rb
@@ -1,5 +1,8 @@
 
 ExceptionNotifier.configure_exception_notifier do |config|
   config[:exception_recipients] = AppConfig.exception_recipients
+  if !AppConfig.exception_sender_address.blank?
+    config[:sender_address] = AppConfig.exception_sender_address
+  end
   config[:view_path] =  'app/views/error'
 end
