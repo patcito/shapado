58d744df49fb042c0ff107c68fffa24a2cd685ce
Patrick Aljord <patcito@gmail.com>
2009-11-16 16:24:29 -0500

    added members.js

---
+++ public/javascripts/modules/members.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    $('#member_user_id').autocomplete('/users', {
+            multiple: false,
+            dataType: 'json',
+            delay: 200,
+            selectFirst: false,
+            parse: function(data) {
+                return $.map(data, function(item) {
+                    return {
+                        data: item,
+                        value: item.login,
+                        result: item.login
+                    };
+                });
+            },
+            formatItem: function(item) {
+                return item.login;
+            }
+        });
+
+})
