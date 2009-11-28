72a6d4be964a55e84a32b02e8a0d23edc5c015b4
David A. Cuadrado <krawek@gmail.com>
2009-11-27 23:27:34 -0500

    set haml version
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- config/environment.rb
+++ config/environment.rb
@@ -28,6 +28,8 @@ Rails::Initializer.run do |config|
   config.gem "mongomapper_ext", :version => "0.0.4", :source => "http://gemcutter.org"
   config.gem "geoip"
   config.gem "uuidtools", :version => "2.1.1"
+  config.gem "haml", :version => "2.2.14"
+
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
