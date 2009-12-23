36d07342341c5bf2717c11211eebff49580e01bb
David A. Cuadrado <krawek@gmail.com>
2009-12-23 15:52:52 -0500

    fix some typos
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -38,10 +38,10 @@ end
 REPUTATION_CONSTRAINS = {"vote_up" => 15, "flag" => 15, "post_images" => 15,
 "comment" => 50, "delete_own_comments" => 50, "vote_down" => 100,
 "create_new_tags" => 100, "post_whithout_limits" => 100, "edit_wiki_post" => 100,
-"remove_advertising" => 200, "vote_open_own_question" => 250, "vote_close_own_question" => 250,
+"remove_advertising" => 200, "vote_to_open_own_question" => 250, "vote_to_close_own_question" => 250,
 "retag_others_questions" => 500, "delete_comments_on_own_questions" => 750,
-"edit_others_posts" => 2000, "view_offensive_counts" => 2000, "vote_open_any_question" => 3000,
-"vote_close_any_question" => 3000, "delete_closed_questions" => 10000, "moderate" => 10000}
+"edit_others_posts" => 2000, "view_offensive_counts" => 2000, "vote_to_open_any_question" => 3000,
+"vote_to_close_any_question" => 3000, "delete_closed_questions" => 10000, "moderate" => 10000}
 
 REPUTATION_CONF = YAML.load_file(reputation_config_file)
 
