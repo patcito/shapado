414c3107d5913f493fa6f6dd90517d7ca7b1930d
David A. Cuadrado <krawek@gmail.com>
2009-12-30 16:27:04 -0500

    improve badges system
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -2,17 +2,28 @@ class Badge
   include MongoMapper::Document
 
   TYPES = %w[gold silver bronce]
+  TOKENS = %w[service_medal merit_medal effort_medal popstar rockstar addict
+              noob fanatic tutor critic commentator student famous_question
+              favorite_question good_question good_answer]
 
   key :_id, String
   key :user_id, String, :required => true
   belongs_to :user
 
-  key :name, String, :required => true
+  key :token, String, :required => true, :index => true
   key :type, String, :required => true
 
+  key :_type, String
+  timestamps!
+
   validates_inclusion_of :type,  :within => TYPES
+  validates_inclusion_of :token, :within => TOKENS
 
   def self.gold_badges
     self.find_all_by_type("gold")
+  end
+
+  def name
+    @name ||= I18n.t("badges.#{self.token}", :default => self.token.titleize) if self.token
   end
 end
