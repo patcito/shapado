81865137949ceee0942046070964701bd838b81d
David A. Cuadrado <krawek@gmail.com>
2010-01-03 18:24:53 -0500

    top users widget; improve style
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/models/widgets/top_users_widget.rb
+++ app/models/widgets/top_users_widget.rb
@@ -1,14 +1,14 @@
-class UsersWidget < Widget
+class TopUsersWidget < Widget
   before_validation_on_create :set_name
 
-  def recent_users(group)
-    User.all(:order => "created_at desc",
+  def top_users(group)
+    User.all(:order => "reputation.#{group.id} desc",
              :conditions => {:"reputation.#{group.id}" => {:"$exists" => true}},
              :limit => 5)
   end
 
   protected
   def set_name
-    self[:name] ||= "users"
+    self[:name] ||= "top_users"
   end
 end
