9df2f9cb703a644c046c935e7364fe138d423d76
David A. Cuadrado <krawek@gmail.com>
2010-01-06 16:05:11 -0500

    fix initializer
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -1,22 +1,13 @@
-# config/initializers/available_locales.rb
-#
-# Get loaded locales conveniently
-# See http://rails-i18n.org/wiki/pages/i18n-available_locales
-module I18n
-  class << self
-    def available_locales; backend.available_locales; end
-  end
-  module Backend
-    class Simple
-      def available_locales; translations.keys.collect { |l| l.to_s }.sort; end
-    end
-  end
-end
+I18n.backend.store_translations 'es-AR', {}
+I18n.backend.store_translations 'en', {}
+I18n.backend.store_translations 'fr', {}
+I18n.backend.store_translations 'pt-PT', {}
+
+I18n.load_path << Dir[ File.join(RAILS_ROOT, 'config', 'locales', '**', '*.{rb,yml}') ]
 
 # You need to "force-initialize" loaded locales
 I18n.backend.send(:init_translations)
 
 AVAILABLE_LOCALES = I18n.backend.available_locales
-AVAILABLE_LANGUAGES = I18n.backend.available_locales.map { |l| l.split("-").first}.uniq
+AVAILABLE_LANGUAGES = I18n.backend.available_locales.map { |l| l.to_s.split("-").first}.uniq
 RAILS_DEFAULT_LOGGER.debug "* Loaded locales: #{AVAILABLE_LOCALES.inspect}"
-
