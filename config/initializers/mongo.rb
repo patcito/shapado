82e50cf27321cd6a58e50938fb1400600b5d3303
David A. Cuadrado <krawek@gmail.com>
2010-01-07 09:47:31 -0500

    reorder initializers; make sure indexes are created
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -2,4 +2,9 @@ require 'mm-paginate'
 
 MongoMapper.connection = Mongo::Connection.new(nil, nil, :auto_reconnect => true)
 MongoMapper.database = "shapado-#{Rails.env}"
+
+Dir.glob("#{RAILS_ROOT}/app/models/**/*") do |model_path|
+  puts File.basename(model_path, ".rb").classify.constantize
+end
+
 MongoMapper.ensure_indexes!
