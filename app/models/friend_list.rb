f7a9f41ae41331c045cc58d65586f61f4cb0d200
David A. Cuadrado <krawek@gmail.com>
2010-01-19 09:55:45 -0500

    add missing model
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

---
+++ app/models/friend_list.rb
@@ -0,0 +1,11 @@
+class FriendList
+  include MongoMapper::Document
+
+  key :_id, String
+
+  key :follower_ids, Array
+  many :followers, :in => :follower_ids, :class_name => "User"
+
+  key :following_ids, Array
+  many :following, :in => :followers_ids, :class_name => "User"
+end
