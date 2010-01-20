e8b2352c4e3bed11015f901188782cfad8942b33
David A. Cuadrado <krawek@gmail.com>
2010-01-20 11:08:47 -0500

    add support for phusion passenger
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -3,6 +3,12 @@ require 'mm-paginate'
 MongoMapper.connection = Mongo::Connection.new(nil, nil, :auto_reconnect => true)
 MongoMapper.database = "shapado-#{Rails.env}"
 
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    MongoMapper.connection.connect_to_master if forked
+  end
+end
+
 Dir.glob("#{RAILS_ROOT}/app/models/**/*.rb") do |model_path|
   File.basename(model_path, ".rb").classify.constantize
 end
