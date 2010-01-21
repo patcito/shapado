414ab3e7d330e6378b1f446e256c5f866b7d2e1a
Jorge H. Cuadrado <kuadrosxx@gmail.com>
2010-01-21 14:19:33 -0500

    Fix redirection after login
    Signed-off-by: Jorge H. Cuadrado <kuadrosxx@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
   # render new.rhtml
   def new
-    session[:return_to] = request.referrer if !request.referrer.blank?
+    session[:return_to] ||= request.request_uri if request.get?
   end
 
   def create
