5bc31a05ae3fb22a961a0f98296c65d19571acdc
David A. Cuadrado <krawek@gmail.com>
2010-02-18 23:57:27 -0500

    minor fix
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -4,7 +4,7 @@ class Ad
   POSITIONS=[["context_panel","context_panel"],["header","header"],["footer","footer"],["content","content"]]
   slug_key :name
   key :name
-  key :group_id, ObjectId
+  key :group_id, String
   key :position, String
   key :_type, String
   key :code, String
