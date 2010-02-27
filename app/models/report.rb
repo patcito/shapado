e3961152df8991bbef6e97a5cf0b40e05c3c2ea9
David A. Cuadrado <krawek@gmail.com>
2010-02-27 03:19:25 -0500

    add missing model; i18n
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

---
+++ app/models/report.rb
@@ -0,0 +1,17 @@
+class Report
+  attr_reader :group, :since
+  attr_reader :questions, :answers, :users, :badges, :votes
+
+  def initialize(group, since = Time.now.yesterday)
+    @group = group
+    @since = since
+
+    @questions = group.questions.count(:created_at => {:$gt => since})
+    @answers = group.answers.count(:created_at => {:$gt => since})
+
+    @users = User.count(:$exists => "reputation.#{group.id}")
+    @badges = group.badges.count(:created_at => {:$gt => since})
+
+    @votes = group.votes.count(:created_at => {:$gt => since})
+  end
+end
