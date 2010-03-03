c992a486db71baacaae032d8b15af0e919aaf372
Jorge H. Cuadrado <kuadrosxx@gmail.com>
2010-03-02 23:23:45 -0500

    improve Question#now using javascript * hide body textarea and highlight help box
    Signed-off-by: Jorge H. Cuadrado <kuadrosxx@gmail.com>

--- public/javascripts/modules/new_question.js
+++ public/javascripts/modules/new_question.js
@@ -1,5 +1,17 @@
 
 $(document).ready(function() {
+
+  $("#question_body").hide();
+  $("#more_details").show();
+
+  $("#more_details").click(function() {
+    $("#question_body").toggle();
+  });
+
+  $(".text_field#question_title").focus( function() {
+    highlightEffect($("#sidebar .help"))
+  });
+
   $("#ask_question").search({ url : "/questions/related_questions.js",
                               target : $("#related_questions"),
                               behaviour : "focusout",
