ab99ae1ed97f3bed0eaeded7e4971d1021d0e0ad
David A. Cuadrado <krawek@gmail.com>
2010-03-04 13:50:24 -0500

    update fancy-buttons
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,5 +1,5 @@
 require 'compass'
 # If you have any compass plugins, require them here.
-Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.config"))
+Compass.add_project_configuration(File.join(RAILS_ROOT, "config", "compass.config"))
 Compass.configuration.environment = RAILS_ENV.to_sym
 Compass.configure_sass_plugin!
