04d9c04500e1071453bdb4d58dae42b6ae576a25
David A. Cuadrado <krawek@gmail.com>
2010-03-13 03:46:26 -0500

    code cleanup and fix some badges
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/actors/judge/activities.rb
+++ app/actors/judge/activities.rb
@@ -5,13 +5,13 @@ module JudgeActions
       user = User.find(user_id, :select => [:_id])
       group = Group.find(group_id, :select => [:_id])
 
-      days = user.stats(:activity_days).activity_days[group_id]
-      if days > 8 && user.find_badge_on(group, "shapado").nil?
-        create_badge(user, group, :token => "shapado", :group_id => group_id)
-      elsif days > 20 && user.find_badge_on(group, "addict").nil?
-        create_badge(user, group, :token => "addict", :group_id => group_id)
-      elsif days > 100 && user.find_badge_on(group, "fanatic").nil?
-        create_badge(user, group, :token => "fanatic", :group_id => group_id)
+      days = user.config_for(group).activity_days
+      if days > 100
+        create_badge(user, group, :token => "fanatic", :unique => true)
+      elsif days > 20
+        create_badge(user, group, :token => "addict", :unique => true)
+      elsif days > 8
+        create_badge(user, group, :token => "shapado", :unique => true)
       end
     end
 
@@ -19,11 +19,11 @@ module JudgeActions
       answer = Answer.find(payload.first)
       user = answer.updated_by
 
-      user.find_badge_on(answer.group, "editor") || create_badge(user, answer.group, :token => "editor", :group_id => answer.group_id)
+      create_badge(user, answer.group, :token => "editor", :unique => true)
     end
 
     def on_destroy_answer(payload)
-      deleter = User.find(payload.first)
+      deleter = User.find!(payload.first)
       attributes = payload.last
       group = Group.find(attributes["group_id"])
 
@@ -40,13 +40,13 @@ module JudgeActions
 
     def on_comment(payload)
       comment_id = payload.first
-      comment = Comment.find(comment_id)
+      comment = Comment.find!(comment_id)
       commentable = comment.commentable
       group = comment.group
       user = comment.user
 
       if user.comments.count(:group_id => comment.group_id, :_type => {:$ne => "Answer"}) >= 10
-        user.find_badge_on(group, "commentator") || create_badge(user, group, :token => "commentator", :group_id => group.id, :source => comment)
+        create_badge(user, group, :token => "commentator", :source => comment, :unique => true)
       end
     end
 
@@ -55,16 +55,16 @@ module JudgeActions
       followed = User.find(payload.shift)
       group = Group.find(payload.shift)
 
-      if follower.following_count < 5 && follower.find_badge_on(group, "friendly").nil?
-        create_badge(follower, group, :token => "friendly",:group_id => group.id, :source => followed)
+      if follower.following_count > 1
+        create_badge(follower, group, :token => "friendly",:source => followed, :unique => true)
       end
 
-      if followed.followers_count >= 10 && followed.find_badge_on(group, "interesting_person").nil?
-        create_badge(followed, group, :token => "interesting_person",:group_id => group.id)
-      elsif followed.followers_count >= 50 && followed.find_badge_on(group, "popular_person").nil?
-        create_badge(followed, group, :token => "popular_person",:group_id => group.id)
-      elsif followed.followers_count >= 100 && followed.find_badge_on(group, "celebrity").nil?
-        create_badge(followed, group, :token => "celebrity",:group_id => group.id)
+      if followed.followers_count >= 100
+        create_badge(followed, group, :token => "celebrity",:unique => true)
+      elsif followed.followers_count >= 50
+        create_badge(followed, group, :token => "popular_person",:unique => true)
+      elsif followed.followers_count >= 10
+        create_badge(followed, group, :token => "interesting_person",:unique => true)
       end
     end
 
