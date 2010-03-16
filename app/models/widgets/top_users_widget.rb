72d44ba1a77a851fba51b4e1a92595e3688d4bf4
David A. Cuadrado <krawek@gmail.com>
2010-03-16 03:06:22 -0500

    fix user widgets
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/models/widgets/top_users_widget.rb
+++ app/models/widgets/top_users_widget.rb
@@ -2,9 +2,9 @@ class TopUsersWidget < Widget
   before_validation_on_create :set_name
 
   def top_users(group)
-    User.all(:order => "reputation.#{group.id} desc",
-             :conditions => {:"reputation.#{group.id}" => {:"$exists" => true}},
-             :limit => 5)
+    group.users(:order => "reputation.#{group.id} desc",
+                :per_page => 5,
+                :page => 1)
   end
 
   protected
--- app/models/widgets/users_widget.rb
+++ app/models/widgets/users_widget.rb
@@ -2,9 +2,9 @@ class UsersWidget < Widget
   before_validation_on_create :set_name
 
   def recent_users(group)
-    User.all(:order => "created_at desc",
-             :conditions => {:"reputation.#{group.id}" => {:"$exists" => true}},
-             :limit => 5)
+    group.users(:order => "created_at desc",
+                :per_page => 5,
+                :page => 1)
   end
 
   protected
