b366beff2eb37d5e3972b7a12a95f2371f6d73a7
David A. Cuadrado <krawek@gmail.com>
2010-04-03 19:21:46 -0500

    add unicorn config
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

---
+++ config/unicorn/bluepill.rb
@@ -0,0 +1,25 @@
+RAILS_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
+rails_env = ENV["RAILS_ENV"] || 'production'
+
+Bluepill.application("shapado", :log_file => RAILS_ROOT+"/log/bluepill.log") do |app|
+  app.process("unicorn-shapado") do |process|
+    process.pid_file = File.join(RAILS_ROOT, 'tmp', 'pids', 'unicorn.pid')
+    process.working_dir = RAILS_ROOT
+
+    process.start_command = "unicorn_rails -Dc #{RAILS_ROOT}/config/unicorn/unicorn.rb -E #{rails_env}"
+    process.stop_command = "kill -QUIT {{PID}}"
+    process.restart_command = "kill -USR2 {{PID}}"
+
+    process.start_grace_time = 8.seconds
+    process.stop_grace_time = 5.seconds
+    process.restart_grace_time = 13.seconds
+
+    process.monitor_children do |child_process|
+      child_process.stop_command = "kill -QUIT {{PID}}"
+
+      child_process.checks :mem_usage, :every => 15.seconds, :below => 150.megabytes, :times => [3,4], :fires => :stop
+      child_process.checks :cpu_usage, :every => 15.seconds, :below => 90, :times => [3,4], :fires => :stop
+    end
+  end
+end
+
---
+++ config/unicorn/unicorn.rb
@@ -0,0 +1,77 @@
+WD = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
+
+# Use at least one worker per core
+worker_processes 4
+
+# Help ensure your application will always spawn in the symlinked "current" directory that Capistrano sets up
+working_directory WD
+
+# Listen on a Unix domain socket, use the default backlog size
+listen "/tmp/unicorn-shapado.sock", :backlog => 1024
+
+# Nuke workers after 30 seconds instead of 60 seconds (the default)
+timeout 30
+
+# Lets keep our process id's in one place for simplicity
+pid WD + "/tmp/pids/unicorn.pid"
+
+# Logs are very useful for trouble shooting, use them
+stderr_path WD+"/log/unicorn.stderr.log"
+stdout_path WD+"/log/unicorn.stdout.log"
+
+# Use "preload_app true"
+preload_app true
+
+# GC.respond_to?(:copy_on_write_friendly=) and
+#   GC.copy_on_write_friendly = true
+
+before_fork do |server, worker|
+  old_pid = WD+'/tmp/unicorn.pid.oldbin'
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+
+  # per-process listener ports for debugging/admin/migrations
+  # addr = "127.0.0.1:#{9293 + worker.nr}"
+  # server.listen(addr, :tries => -1, :delay => 5, :tcp_nopush => true)
+
+  # the following is *required* for Rails + "preload_app true"
+
+  MongoMapper.connection.connect_to_master
+
+  # if preload_app is true, then you may also want to check and
+  # restart any other shared sockets/descriptors such as Memcached,
+  # and Redis. TokyoCabinet file handles are safe to reuse
+  # between any number of forked children (assuming your kernel
+  # correctly implements pread()/pwrite() system calls)
+
+  # Unicorn master is started as root, which is fine, but let's
+  # drop the workers to your user/group
+  begin
+    uid, gid = Process.euid, Process.egid
+
+    target_uid = File.stat(RAILS_ROOT).uid
+    user = Etc.getpwuid(target_uid).name
+
+    target_gid = File.stat(RAILS_ROOT).gid
+    group = Etc.getgrgid(target_gid).name
+
+    worker.tmp.chown(target_uid, target_gid)
+    if uid != target_uid || gid != target_gid
+      Process.initgroups(user, target_gid)
+      Process::GID.change_privilege(target_gid)
+      Process::UID.change_privilege(target_uid)
+    end
+  rescue => e
+    STDERR.puts "cannot change privileges on #{RAILS_ENV} environment"
+    STDERR.puts "  #{e}"
+  end
+
+end
