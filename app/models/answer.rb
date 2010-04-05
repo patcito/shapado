738150e550a110eb27ec16e351dd23f9f6485062
Jorge H. Cuadrado <kuadrosxx@gmail.com>
2010-04-04 21:31:47 -0500

    Fix comments order
    Signed-off-by: Jorge H. Cuadrado <kuadrosxx@gmail.com>

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -23,7 +23,7 @@ class Answer < Comment
   has_many :votes, :as => "voteable", :dependent => :destroy
   has_many :flags, :as => "flaggeable", :dependent => :destroy
 
-  has_many :comments, :foreign_key => "commentable_id", :class_name => "Comment", :dependent => :destroy
+  has_many :comments, :foreign_key => "commentable_id", :class_name => "Comment", :order => "created_at asc", :dependent => :destroy
 
   validates_presence_of :user_id
   validates_presence_of :question_id
