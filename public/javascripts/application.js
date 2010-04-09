0b111ba97b2a7635e6b7634b7eea87364de57a78
Patrick Aljord <patcito@gmail.com>
2010-04-09 15:31:25 -0500

    use ajax for quick vote

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -30,6 +30,31 @@ $(document).ready(function() {
       selectFirst: false,
       extraParams: {'format' : 'js'},
   });
+
+  $(".quick-vote-button").live("click", function(event) {
+    var btn = $(this);
+    btn.hide();
+    var src = btn.attr('src');
+    if (src.indexOf('/images/dialog-ok.png') == 0){
+      var btn_name = $(this).attr("name")
+      var form = $(this).parents("form");
+      $.post(form.attr("action"), form.serialize()+"&"+btn_name+"=1", function(data){
+        if(data.success){
+          btn.parents('.item').find('.votes .counter').text(data.average);
+          btn.attr('src', '/images/dialog-ok-apply.png');
+          showMessage(data.message, "notice")
+        } else {
+          showMessage(data.message, "error")
+          if(data.status == "unauthenticate") {
+            window.location="/users/login"
+          }
+        }
+        btn.show();
+      }, "json");
+    }
+    return false;
+  });
+
 })
 
 function manageAjaxError(XMLHttpRequest, textStatus, errorThrown) {
