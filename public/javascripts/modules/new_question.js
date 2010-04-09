e645ad6dda1ca7cf69c1bf1c4a441c3007bbb942
Jorge H. Cuadrado <kuadrosxx@gmail.com>
2010-04-09 02:53:15 -0500

    Show related questions on quick question form
    Signed-off-by: Jorge H. Cuadrado <kuadrosxx@gmail.com>

--- public/javascripts/modules/new_question.js
+++ public/javascripts/modules/new_question.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
   $("#ask_question").search({ url : "/questions/related_questions.js",
                               target : $("#related_questions"),
                               behaviour : "focusout",
-                              timeout : 10,
+                              timeout : 2500,
                               extraParams : { 'format' : 'js', 'per_page' : 5 },
                               success: function() {
                                 $("label#rqlabel").show();
