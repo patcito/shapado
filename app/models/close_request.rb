2081db6f315bc4020e74a09f185670e8d8d9fa1e
Jorge H. Cuadrado <kuadrosxx@gmail.com>
2010-04-20 03:18:06 -0500

    show reason for closing
    Signed-off-by: Jorge H. Cuadrado <kuadrosxx@gmail.com>

--- app/models/close_request.rb
+++ app/models/close_request.rb
@@ -30,14 +30,14 @@ class CloseRequest
       reputation = self._root_document.group.reputation_constrains["vote_to_close_own_question"]
       self.errors.add(:reputation, I18n.t("users.messages.errors.reputation_needed",
                                           :min_reputation => reputation,
-                                          :action => I18n.t("users.actions.close_own_question")))
+                                          :action => I18n.t("users.actions.vote_to_close_own_question")))
       return false
     end
     unless self.user.can_vote_to_close_any_question_on?(self._root_document.group)
       reputation = self._root_document.group.reputation_constrains["vote_to_close_any_question"]
             self.errors.add(:reputation, I18n.t("users.messages.errors.reputation_needed",
                                           :min_reputation => reputation,
-                                          :action => I18n.t("users.actions.close_any_question")))
+                                          :action => I18n.t("users.actions.vote_to_close_any_question")))
       return false
     end
     return true
