dcc8ea0e24d0cf8a166f22818a629106120bf4f6
David A. Cuadrado <krawek@gmail.com>
2010-05-01 13:07:50 -0500

    update dependencies
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- config/environment.rb
+++ config/environment.rb
@@ -19,14 +19,14 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "rdiscount", :version => "1.5.8"
-  config.gem "jnunemaker-validatable", :version => "1.8.1", :lib => "validatable"
+  config.gem "rdiscount", :version => "1.6.3.1"
+  config.gem "jnunemaker-validatable", :version => "1.8.3", :lib => "validatable"
   config.gem "mongo_mapper", :version => "0.7.3", :source => "http://gemcutter.org"
-  config.gem "compass", :version => "0.10.0.pre8", :lib => "compass", :source => "http://gemcutter.org"
-  config.gem "fancy-buttons", :version => "0.4.1", :source => "http://gemcutter.org"
+  config.gem "compass", :version => "0.10.0.rc4", :lib => "compass", :source => "http://gemcutter.org"
+  config.gem "fancy-buttons", :version => "0.5.1", :source => "http://gemcutter.org"
   config.gem "compass-colors", :version => "0.3.1", :source => "http://gemcutter.org"
   config.gem "ruby-stemmer", :version => ">=0.5.3", :lib => "lingua/stemmer"
-  config.gem "mongomapper_ext", :version => "0.2.3", :source => "http://gemcutter.org"
+  config.gem "mongomapper_ext", :version => "0.2.4", :source => "http://gemcutter.org"
   config.gem "geoip"
   config.gem "whatlanguage", :version => "1.0.0"
   config.gem "uuidtools", :version => "2.1.1"
@@ -35,7 +35,7 @@ Rails::Initializer.run do |config|
   config.gem 'super_exception_notifier', :version => '~> 2.0.0', :lib => 'exception_notifier'
   config.gem "warden", :version => "0.10.3"
   config.gem "devise", :version => "1.0.5"
-  config.gem "twitter-text", :version => "1.0.4"
+  config.gem "twitter-text", :version => "1.1.1"
 
   if File.exist?(RAILS_ROOT+"/config/facebooker.yml")
     config.gem "devise_facebook_connectable", :version => "0.1.9"
