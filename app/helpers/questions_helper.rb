9920bcc31142876d4270f24a2ddbc98fff193507
~spacecowboy <simon.probert1@googlemail.com>
2010-05-07 17:06:50 +1000

    Fixes to the Share URLs

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -12,15 +12,18 @@ module QuestionsHelper
       when :identica
         url = "http://identi.ca/notice/new?status_textarea=#{microblogging_message(question)}"
       when :shapado
-        url = "http://shapado.com?"
+        url = "http://shapado.com/questions/new?question[title]="+(question.title)+"&question[tags]=#{current_group.name},share&question[body]=#{h(question.body)}%20|%20[More...](#{h(question_path(question, :only_path =>false))})"
       when :twitter
         url = "http://twitter.com/?status=#{microblogging_message(question)}"
       when :facebook
-        url = "http://www.facebook.com/sharer.php?u=#{microblogging_message(question)}&t=TEXTO"
+        url = "http://www.facebook.com/sharer.php?u=#{question_path(question, :only_path =>false)}&t=#{h(question.title)}"
+
+#SP: The LinkedIn url needs to be url encoded but I can't quite seem to figure it out
+
       when :linkedin
-        url = "http://linkedin.com/shareArticle?mini=true&url="+question_path(question, :only_path =>false)+"&title=#{h(question.title)}&summary=#{h(question.body)}&source=Shapado"
+        url = "http://linkedin.com/shareArticle?mini=true&url=&title=#{h(question.title)}&summary=#{h(question.body)}&source=#{current_group.name}"
       when :think
-        url = "http://beta.think.it:3000?"
+        url = "http://beta.think.it:3000/thoughts/new?question[title]="+(question.title)+"&question[tags]=#{current_group.name},share&question[body]=#{h(question.body)}%20|%20[More...](#{h(question_path(question, :only_path =>false))})"
     end
     url
   end
