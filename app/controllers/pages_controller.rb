976bc27a7e8f010c53066acc805d410a3b474343
David A. Cuadrado <krawek@gmail.com>
2010-05-08 19:25:08 -0500

    make pages index visible
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,5 @@
 class PagesController < ApplicationController
-  before_filter :login_required, :except => [:show]
-  before_filter :moderator_required, :only => [:index]
-
+  before_filter :login_required, :except => [:show, :index]
   before_filter :check_page_permissions, :only => [:new, :create, :edit, :update, :destroy]
 
   layout :set_layout
@@ -11,7 +9,15 @@ class PagesController < ApplicationController
   # GET /pages
   # GET /pages.json
   def index
-    @pages = current_group.pages.paginate(:page => params[:page], :per_page => params[:per_page] || 25)
+    options = {:page => params[:page], :per_page => params[:per_page] || 25}
+
+    if !logged_in? || !current_user.mod_of?(current_group)
+      options[:wiki] = true
+    end
+
+    @pages = current_group.pages.paginate(options)
+
+    set_page_title("Wiki") # TODO: i18n
 
     respond_to do |format|
       format.html # index.html.haml
@@ -23,6 +29,8 @@ class PagesController < ApplicationController
   # GET /pages/1.json
   def show
     @page = current_group.pages.by_slug(params[:id], :language => Page.current_language) || current_group.pages.by_slug(params[:id])
+
+    set_page_title(@page.title)
 
     respond_to do |format|
       format.html do
@@ -64,8 +72,11 @@ class PagesController < ApplicationController
     if (js = params[:page][:js]) && current_group.has_custom_js && current_user.role_on(current_group) == "owner"
       @page.js = js
     end
+
     @page.group = current_group
     @page.user = current_user
+
+    @page.wiki = true unless current_user.mod_of?(current_group)
 
     respond_to do |format|
       if @page.save
@@ -82,8 +93,10 @@ class PagesController < ApplicationController
   # PUT /pages/1
   # PUT /pages/1.json
   def update
-    @page.safe_update(%w[title body tags wiki language adult_content css js], params[:page])
+    @page.safe_update(%w[title body tags language adult_content css js], params[:page])
     @page.updated_by = current_user
+
+    @page.safe_update(%w[wiki], params[:page]) if current_user.mod_of?(current_group)
 
     respond_to do |format|
       if @page.save
