24abcd9771605945cf1110282a7ec14a79b81799
Patrick Aljord <patcito@gmail.com>
2010-05-12 02:43:24 -0500

    fix fb button and escape microblogs urls

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,7 +2,7 @@ module QuestionsHelper
   def microblogging_message(question)
     message = "#{h(question.title)}"
     message += " "
-    message +=  question_path(question, :only_path =>false)
+    message +=  escape_url(question_path(question, :only_path =>false))
     message
   end
 
@@ -15,12 +15,17 @@ module QuestionsHelper
         url = "http://identi.ca/notice/new?status_textarea=#{microblogging_message(question)}"
       when :facebook
         if current_group.fb_button
-          url = %@<iframe src="http://www.facebook.com/plugins/like.php?href=http%253A%252F%252F#{question_path(question, :only_path =>false)}&amp;layout=button_count&amp;show_faces=true&amp;width=450&amp;action=like&amp;font&amp;colorscheme=light&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe>@
+          url = %@<iframe src="http://www.facebook.com/plugins/like.php?href=#{escape_url(question_path(question, :only_path =>false))}&amp;layout=button_count&amp;show_faces=true&amp;width=450&amp;action=like&amp;font&amp;colorscheme=light&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe>@
         else
-          fb_url = "http://www.facebook.com/sharer.php?u=#{question_path(question, :only_path =>false)}&t=#{question.title}"
+          fb_url = "http://www.facebook.com/sharer.php?u=#{escape_url(question_path(question, :only_path =>false))}&t=#{question.title}"
           url = %@#{image_tag('/images/hi16-app-facebook_microblog.png', :class => 'microblogging')} #{link_to("facebook", fb_url, :rel=>"nofollow external")}@
         end
     end
     url
+  end
+
+  protected
+  def escape_url(url)
+    URI.escape(url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
   end
 end
