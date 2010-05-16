25082351c7c74f0a60b0cbc4d04950e418ee6e21
David A. Cuadrado <krawek@gmail.com>
2010-05-16 17:14:21 -0500

    remove old fb auth
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- config/environment.rb
+++ config/environment.rb
@@ -38,11 +38,7 @@ Rails::Initializer.run do |config|
   config.gem "warden", :version => "0.10.3"
   config.gem "devise", :version => "1.0.5"
   config.gem "twitter-text", :version => "1.1.1"
-
-  if File.exist?(RAILS_ROOT+"/config/facebooker.yml")
-    config.gem "devise_facebook_connectable", :version => "0.1.9"
-    config.gem "facebooker", :version => "1.0.67"
-  end
+  config.gem "oauth2", :version => "0.0.8"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
