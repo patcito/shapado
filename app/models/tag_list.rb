6312679f9350bb76204ea07fd2a8bc3bb62c6333
David A. Cuadrado <krawek@gmail.com>
2010-05-17 01:10:25 -0500

    add missing file
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

---
+++ app/models/tag_list.rb
@@ -0,0 +1,21 @@
+class TagList
+  include MongoMapper::Document
+
+  key :_id, String
+  key :group_id, String
+  belongs_to :group
+
+  key :tags, Hash
+
+  def self.add_tags(group_id, *tags)
+    toinc = {"tags" => {}}
+    tags.each do |tag|
+      toinc["tags.#{tag.gsub('.', '%dot%')}"] = 1
+    end
+    self.collection.update({:group_id => group_id}, {:$inc => toinc}, {:upsert => true})
+  end
+
+  def add_tags(*tags)
+    self.class.add_tags(self.group_id, *tags)
+  end
+end
