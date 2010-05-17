1b066545745f90270ee8a981d2d9aacf8c38e61a
David A. Cuadrado <krawek@gmail.com>
2010-05-16 21:52:17 -0500

    fix some issues with sharing question code; move links to the top of the question
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -20,21 +20,20 @@ module QuestionsHelper
           url = "http://twitter.com/?status=#{microblogging_message(question)}"
         end
       when :identica
-        url = "http://identi.ca/notice/new?status_textarea=#{microblogging_message(question)}" 
+        url = "http://identi.ca/notice/new?status_textarea=#{microblogging_message(question)}"
       when :shapado
-        #SP: The rationale here is that when in a group site, you can share a question with the main site, however if you are on the main site then this share is hidden (no point in referring to iteself after all)
         url = "http://shapado.com/questions/new?question[title]="+(question.title)+"&question[tags]=#{current_group.name},share&question[body]=#{h(question.body)}%20|%20[More...](#{h(question_path(question, :only_path =>false))})"
+      when :linkedin
+        url = "http://linkedin.com/shareArticle?mini=true&url="+escape_url(question_url(question))+"&title=#{h(question.title)}&summary=#{h(question.body)}&source=#{current_group.name}"
+      when :think
+        url = "http://thnik.it/thoughts/new?question[title]="+(question.title)+"&question[tags]=#{current_group.name},share&question[body]=#{h(question.body)}%20|%20[More...](#{h(question_path(question, :only_path =>false))})"
       when :facebook
         if current_group.fb_button
           url = %@<iframe src="http://www.facebook.com/plugins/like.php?href=#{escape_url(question_path(question, :only_path =>false))}&amp;layout=button_count&amp;show_faces=true&amp;width=450&amp;action=like&amp;font&amp;colorscheme=light&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe>@
         else
           fb_url = "http://www.facebook.com/sharer.php?u=#{escape_url(question_path(question, :only_path =>false))}&t=#{question.title}"
-          url = %@#{image_tag('/images/hi16-app-facebook_microblog.png', :class => 'microblogging')} #{link_to("facebook", fb_url, :rel=>"nofollow external")}@
+          url = %@#{image_tag('/images/share/facebook_32.png', :class => 'microblogging')} #{link_to("facebook", fb_url, :rel=>"nofollow external")}@
         end
-      when :linkedin
-        url = "http://linkedin.com/shareArticle?mini=true&url="+URL.escape(question_url(question))+"&title=#{h(question.title)}&summary=#{h(question.body)}&source=#{current_group.name}"
-      when :think
-        url = "http://thnik.it/thoughts/new?question[title]="+(question.title)+"&question[tags]=#{current_group.name},share&question[body]=#{h(question.body)}%20|%20[More...](#{h(question_path(question, :only_path =>false))})"
     end
     url
   end
