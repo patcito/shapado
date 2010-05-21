09eae6e964fea4cb333ca22c047f492b604f7ab0
Juliana Dávila R <gobolindo@gmail.com>
2010-05-21 18:38:14 -0500

    created form for share properties
    Signed-off-by: Juliana Dávila R <gobolindo@gmail.com>

---
+++ app/models/share.rb
@@ -0,0 +1,11 @@
+
+class Share
+  include MongoMapper::EmbeddedDocument
+  key :_id, String
+  key :fb_app_id, String
+  key :fb_secret_key, String
+  key :fb_starts_with, String
+  key :fb_end_with, String
+  key :fb_question_asked, String
+  key :fb_active, Boolean
+end
