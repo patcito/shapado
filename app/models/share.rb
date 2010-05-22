1e0f58cccc5b51dd4a6f91f61fa13af72de15094
David A. Cuadrado <krawek@gmail.com>
2010-05-21 21:59:59 -0500

    fix model
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/models/share.rb
+++ app/models/share.rb
@@ -1,11 +1,12 @@
-
 class Share
   include MongoMapper::EmbeddedDocument
   key :_id, String
   key :fb_app_id, String
   key :fb_secret_key, String
-  key :fb_starts_with, String
-  key :fb_end_with, String
-  key :fb_question_asked, String
   key :fb_active, Boolean, :default => false
+
+  key :starts_with, String
+  key :end_with, String
+
+  key :twitter_user, String
 end
