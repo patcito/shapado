6bf016c3974537d4b17c89e6cebd98c97687ee21
Patrick Aljord <patcito@gmail.com>
2010-05-24 22:32:58 -0500

    refactor show question JS in its own packet and move login.js to it own packet too

--- public/javascripts/modules/show_question.js
+++ public/javascripts/modules/show_question.js
@@ -2,6 +2,10 @@
 $(document).ready(function() {
   $(".forms form.flag_form").hide();
   $("#close_question_form").hide();
+  $('.auto-link').autoVideo();
+  setupEditor();
+  setupWysiwygEditor();
+  $(".markdown code").addClass("prettyprint");
 
   $("form.vote_form button").live("click", function(event) {
     var btn_name = $(this).attr("name")
@@ -343,3 +347,153 @@ $(document).ready(function() {
   });
 });
 
+
+function highlightEffect(object) {
+  if(typeof object != "undefined") {
+    object.fadeOut(400, function() {
+      object.fadeIn(400)
+    });
+  }
+}
+
+function setupWysiwygEditor() {
+  var editor = $("#wysiwyg_editor");
+  if(!editor || editor.length == 0)
+    return;
+
+  editor.wysiwyg({
+    events: {
+      click: function(e) {
+        if(!window.onbeforeunload && !hasStorage()) {
+          //I18n.on_leave_page
+          window.onbeforeunload = function() {return I18n.on_leave_page;};
+        }
+      }
+    }
+  });
+}
+
+function setupEditor() {
+  var editor = $("#markdown_editor");
+  if(!editor || editor.length == 0)
+    return;
+
+  var converter = new Showdown.converter;
+  var timer_id = null;
+
+  var converter_callback = function(value) {
+    $('#markdown_preview')[0].innerHTML = converter.makeHtml(value);
+    //addToLocalStorage(location.href, 'markdown_editor', value);
+    $('#markdown_preview.markdown p code').addClass("prettyprint");
+    if(timer_id)
+      clearTimeout(timer_id);
+
+    timer_id = setTimeout(function(){
+      prettyPrint();
+    }, 500);
+
+  }
+
+  var textarea = editor.TextArea({
+    change: converter_callback
+  });
+
+  var toolbar = $.Toolbar(textarea, {
+    className: "markdown_toolbar"
+  });
+
+  //buttons
+  toolbar.addButton('Italics',function(){
+      this.wrapSelection('*','*');
+  },{
+    id: 'markdown_italics_button'
+  });
+
+  toolbar.addButton('Bold',function(){
+      this.wrapSelection('**','**');
+  },{
+    id: 'markdown_bold_button'
+  });
+
+  toolbar.addButton('Link',function(){
+    var selection = this.getSelection();
+    var response = prompt('Enter Link URL','');
+    if(response == null)
+        return;
+    this.replaceSelection('[' + (selection == '' ? 'Link Text' : selection) + '](' + (response == '' ? 'http://link_url/' : response).replace(/^(?!(f|ht)tps?:\/\/)/,'http://') + ')');
+  },{
+    id: 'markdown_link_button'
+  });
+
+  toolbar.addButton('Image',function(){
+    var selection = this.getSelection();
+    var response = prompt('Enter Image URL','');
+    if(response == null)
+        return;
+    this.replaceSelection('![' + (selection == '' ? 'Image Alt Text' : selection) + '](' + (response == '' ? 'http://image_url/' : response).replace(/^(?!(f|ht)tps?:\/\/)/,'http://') + ')');
+  },{
+    id: 'markdown_image_button'
+  });
+
+  toolbar.addButton('Heading',function(){
+    var selection = this.getSelection();
+    if(selection == '')
+        selection = 'Heading';
+    this.replaceSelection('##'+selection+'##');
+  },{
+    id: 'markdown_heading_button'
+  });
+
+  toolbar.addButton('Unordered List',function(event){
+    this.collectFromEachSelectedLine(function(line){
+        return event.shiftKey ? (line.match(/^\*{2,}/) ? line.replace(/^\*/,'') : line.replace(/^\*\s/,'')) : (line.match(/\*+\s/) ? '*' : '* ') + line;
+    });
+  },{
+    id: 'markdown_unordered_list_button'
+  });
+
+  toolbar.addButton('Ordered List',function(event){
+    var i = 0;
+    this.collectFromEachSelectedLine(function(line){
+        if(!line.match(/^\s+$/)){
+            ++i;
+            return event.shiftKey ? line.replace(/^\d+\.\s/,'') : (line.match(/\d+\.\s/) ? '' : i + '. ') + line;
+        }
+    });
+  },{
+    id: 'markdown_ordered_list_button'
+  });
+
+  toolbar.addButton('Block Quote',function(event){
+    this.collectFromEachSelectedLine(function(line){
+        return event.shiftKey ? line.replace(/^\> /,'') : '> ' + line;
+    });
+  },{
+    id: 'markdown_quote_button'
+  });
+
+  toolbar.addButton('Code Block',function(event){
+    this.collectFromEachSelectedLine(function(line){
+        return event.shiftKey ? line.replace(/    /,'') : '    ' + line;
+    });
+  },{
+    id: 'markdown_code_button'
+  });
+
+  toolbar.addButton('Latex', function(event) {
+    this.wrapSelection('$$','$$');
+  }, {
+    id: 'markdown_latex_button'
+  });
+
+  toolbar.addButton('Help',function(){
+    window.open('http://daringfireball.net/projects/markdown/dingus');
+  },{
+    id: 'markdown_help_button'
+  });
+
+}
+
+$(window).load(function() {
+  prettyPrint();
+});
