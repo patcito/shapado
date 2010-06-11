60f258e2130cb9f13581008618a566e27af35e5a
Siebrand Mazeland <s.mazeland@xs4all.nl>
2010-06-11 02:19:42 +0200

    Load new languages
    Signed-off-by: Siebrand Mazeland <s.mazeland@xs4all.nl>

--- config/initializers/01_locales.rb
+++ config/initializers/01_locales.rb
@@ -4,6 +4,16 @@ I18n.backend.store_translations 'fr', {}
 I18n.backend.store_translations 'pt-PT', {}
 I18n.backend.store_translations 'ja', {}
 I18n.backend.store_translations 'el', {}
+I18n.backend.store_translations 'br', {}
+I18n.backend.store_translations 'de', {}
+I18n.backend.store_translations 'fi', {}
+I18n.backend.store_translations 'gl', {}
+I18n.backend.store_translations 'ko', {}
+I18n.backend.store_translations 'mk', {}
+I18n.backend.store_translations 'nl', {}
+I18n.backend.store_translations 'ru', {}
+I18n.backend.store_translations 'te', {}
+I18n.backend.store_translations 'tl', {}
 
 I18n.load_path << Dir[ File.join(RAILS_ROOT, 'config', 'locales', '**', '*.{rb,yml}') ]
 
