b51f7782fd8d014cc1c15d302b0ae8e0a659b9bc
Siebrand Mazeland <s.mazeland@xs4all.nl>
2010-06-22 09:07:17 +0200

    * add support for new language codes be-TARASK, fr, ia, nb, pms * make the previously commented out languages codes per discussion with patcito

--- config/initializers/01_locales.rb
+++ config/initializers/01_locales.rb
@@ -4,22 +4,22 @@ I18n.backend.store_translations 'fr', {}
 I18n.backend.store_translations 'pt-PT', {}
 I18n.backend.store_translations 'ja', {}
 I18n.backend.store_translations 'el', {}
-
-## We need to remove those language for now
-## until we fix the mails translations so that
-## translatewiki can take care of them
-
-#I18n.backend.store_translations 'br', {}
-#I18n.backend.store_translations 'de', {}
-#I18n.backend.store_translations 'fi', {}
-#I18n.backend.store_translations 'gl', {}
-#I18n.backend.store_translations 'ko', {}
-#I18n.backend.store_translations 'lb', {}
-#I18n.backend.store_translations 'mk', {}
-#I18n.backend.store_translations 'nl', {}
-#I18n.backend.store_translations 'ru', {}
-#I18n.backend.store_translations 'te', {}
-#I18n.backend.store_translations 'tl', {}
+I18n.backend.store_translations 'be-TARASK', {}
+I18n.backend.store_translations 'br', {}
+I18n.backend.store_translations 'de', {}
+I18n.backend.store_translations 'fi', {}
+I18n.backend.store_translations 'fr', {}
+I18n.backend.store_translations 'gl', {}
+I18n.backend.store_translations 'ia', {}
+I18n.backend.store_translations 'ko', {}
+I18n.backend.store_translations 'lb', {}
+I18n.backend.store_translations 'mk', {}
+I18n.backend.store_translations 'nl', {}
+I18n.backend.store_translations 'nb', {}
+I18n.backend.store_translations 'pms', {}
+I18n.backend.store_translations 'ru', {}
+I18n.backend.store_translations 'te', {}
+I18n.backend.store_translations 'tl', {}
 
 I18n.load_path << Dir[ File.join(RAILS_ROOT, 'config', 'locales', '**', '*.{rb,yml}') ]
 
@@ -35,4 +35,3 @@ DEFAULT_USER_LANGUAGES = ['en', 'es-419', 'fr', 'pt-PT', 'ja', 'el', 'de', 'ko',
 
 
 RAILS_DEFAULT_LOGGER.debug "* Loaded locales: #{AVAILABLE_LOCALES.inspect}"
-
