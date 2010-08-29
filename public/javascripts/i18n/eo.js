2a53a5f7b70c124d770e3fe1c2002af84ce817b3
Siebrand Mazeland <s.mazeland@xs4all.nl>
2010-08-30 00:16:55 +0200

    Localisation updates from http://translatewiki.net

---
+++ public/javascripts/i18n/eo.js
@@ -0,0 +1,11 @@
+/** Messages for Esperanto (Esperanto)
+ *  Exported from translatewiki.net
+ *
+ * Translators:
+ *  - Darkweasel
+ */
+
+var I18n = {
+	on_leave_page: "Vi eble perdos viajn ŝanĝojn"
+};
+
---
+++ public/javascripts/i18n/it.js
@@ -0,0 +1,11 @@
+/** Messages for Italian (Italiano)
+ *  Exported from translatewiki.net
+ *
+ * Translators:
+ *  - Gianfranco
+ */
+
+var I18n = {
+	on_leave_page: "Le tue modifiche potrebbero andare perse"
+};
+
