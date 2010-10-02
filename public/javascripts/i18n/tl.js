e3211d0fe31f14bc8e570319f28cedda2b421527
Siebrand Mazeland <s.mazeland@xs4all.nl>
2010-10-02 23:03:07 +0200

    Localisation updates from http://translatewiki.net

---
+++ public/javascripts/i18n/tl.js
@@ -0,0 +1,11 @@
+/** Messages for Tagalog (Tagalog)
+ *  Exported from translatewiki.net
+ *
+ * Translators:
+ *  - AnakngAraw
+ */
+
+var I18n = {
+	on_leave_page: "Maaari mawala ang mga binago mo"
+};
+
