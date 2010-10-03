37492f7bef2b20fb023d3b164778e65f21c2eba6
David A. Cuadrado <krawek@gmail.com>
2010-10-02 21:59:49 -0500

    expire fragment caches correctly
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- lib/sweepers.rb
+++ lib/sweepers.rb
@@ -1,21 +1,21 @@
 module Sweepers
   def sweep_question_views
-    expire_fragment("tag_cloud_#{current_group.id}")
+    expire_fragment(/tag_cloud_#{current_group.id}/)
   end
 
   def sweep_answer_views
   end
 
   def sweep_user_views
-    expire_fragment("new_users_#{current_group.id}")
+    expire_fragment(/new_users_#{current_group.id}/)
   end
 
   def sweep_question(question)
-    expire_fragment("question_on_index_#{question.id}*")
-    expire_fragment("mini_question_on_index_#{question.id}*")
+    expire_fragment(/question_on_index_#{question.id}/)
+    expire_fragment(/mini_question_on_index_#{question.id}/)
   end
 
   def sweep_new_users(group)
-    expire_fragment("new_users_#{group.id}*")
+    expire_fragment(/new_users_#{group.id}/)
   end
 end
