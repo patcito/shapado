0dc262a80677a5c8b1f752c2374de43d875dff28
Siebrand Mazeland <s.mazeland@xs4all.nl>
2010-10-19 00:17:03 +0200

    Localisation updates from http://translatewiki.net

---
+++ public/javascripts/i18n/da.js
@@ -0,0 +1,11 @@
+/** Messages for Danish (Dansk)
+ *  Exported from translatewiki.net
+ *
+ * Translators:
+ *  - Emilkris33
+ */
+
+var I18n = {
+	on_leave_page: "Du kan miste dine ændringer"
+};
+
