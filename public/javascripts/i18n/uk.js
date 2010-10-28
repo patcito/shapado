e8dfc0dfe8d19ad1a983bf3e635de8dc16a7eee6
Siebrand Mazeland <s.mazeland@xs4all.nl>
2010-10-29 01:37:08 +0200

    Localisation updates from http://translatewiki.net

---
+++ public/javascripts/i18n/uk.js
@@ -0,0 +1,11 @@
+/** Messages for Ukrainian (Українська)
+ *  Exported from translatewiki.net
+ *
+ * Translators:
+ *  - Тест
+ */
+
+var I18n = {
+	on_leave_page: "Ви можете втратити ваші зміни"
+};
+
