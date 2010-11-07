4886fa6a3bae45334237266259d9f17af72ded82
Siebrand Mazeland <s.mazeland@xs4all.nl>
2010-11-07 22:03:08 +0100

    Localisation updates from http://translatewiki.net.

---
+++ public/javascripts/i18n/fi.js
@@ -0,0 +1,11 @@
+/** Messages for Finnish (Suomi)
+ *  Exported from translatewiki.net
+ *
+ * Translators:
+ *  - Centerlink
+ */
+
+var I18n = {
+	on_leave_page: "Saatat menettää tekemäsi muutokset"
+};
+
