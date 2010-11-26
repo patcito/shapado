6636d6ff70f80b006183d0607550d8f78dcf990b
David A. Cuadrado <krawek@gmail.com>
2010-11-25 23:24:27 -0500

    refactor Notifier
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -3,65 +3,49 @@ class Notifier < ActionMailer::Base
   layout "notifications"
 
   def give_advice(user, group, question, following = false)
-    domain = group ? group.domain : AppConfig.domain
-    @language = language_for(user)
-    set_locale @language
-    scope = "mailers.notifications.give_advice"
+    setup_email(group, user) do
+      scope = "mailers.notifications.give_advice"
 
-    from from_email(group)
-    recipients user.email
+      if following
+        subject I18n.t("friend_subject", :scope => scope, :question_title => question.title, :locale => @language)
+      else
+        subject I18n.t("subject", :scope => scope, :question_title => question.title, :locale => @language)
+      end
 
-    if following
-      subject I18n.t("friend_subject", :scope => scope, :question_title => question.title, :locale => @language)
-    else
-      subject I18n.t("subject", :scope => scope, :question_title => question.title, :locale => @language)
+      body   :user => user, :question => question,
+      :group => group, :domain => domain_for(group),
+      :following => following
     end
-    sent_on Time.now
-    body   :user => user, :question => question,
-    :group => group, :domain => domain,
-    :following => following
   end
 
   def new_answer(user, group, answer, following = false)
-    @language = language_for(user)
-    set_locale @language
+    setup_email(group, user) do
+      scope = "mailers.notifications.new_answer"
+      if user == answer.question.user
+        @subject = I18n.t("subject_owner", :scope => scope,
+                          :title => answer.question.title,
+                          :login => answer.user.login, :locale => @language)
+      elsif following
+        @subject = I18n.t("subject_friend", :scope => scope,
+                          :title => answer.question.title,
+                          :login => answer.user.login, :locale => @language)
+      else
+        @subject = I18n.t("subject_other", :scope => scope,
+                          :title => answer.question.title,
+                          :login => answer.user.login, :locale => @language)
+      end
 
-    scope = "mailers.notifications.new_answer"
-    if user == answer.question.user
-      @subject = I18n.t("subject_owner", :scope => scope,
-                        :title => answer.question.title,
-                        :login => answer.user.login, :locale => @language)
-    elsif following
-      @subject = I18n.t("subject_friend", :scope => scope,
-                        :title => answer.question.title,
-                        :login => answer.user.login, :locale => @language)
-    else
-      @subject = I18n.t("subject_other", :scope => scope,
-                        :title => answer.question.title,
-                        :login => answer.user.login, :locale => @language)
+      subject @subject
+      body   :user => user, :answer => answer, :question => answer.question,
+      :group => group, :domain => domain_for(group)
     end
-
-    recipients user.email
-    domain = group ? group.domain : AppConfig.domain
-    from from_email(group)
-    subject @subject
-    sent_on Time.now
-    body   :user => user, :answer => answer, :question => answer.question,
-    :group => group, :domain => domain
-
   end
 
   def new_comment(group, comment, user, question)
-    domain = group ? group.domain : AppConfig.domain
-    recipients user.email
-    @language = language_for(user)
-    set_locale @language
-
-    from from_email(group)
-    subject I18n.t("mailers.notifications.new_comment.subject", :login => comment.user.login, :group => group.name, :locale => @language)
-    sent_on Time.now
-
-    body :user => user, :comment => comment, :question => question, :group => group, :domain => domain
+    setup_email(group, user) do
+      subject I18n.t("mailers.notifications.new_comment.subject", :login => comment.user.login, :group => group.name, :locale => @language)
+      body :user => user, :comment => comment, :question => question, :group => group, :domain => domain_for(group)
+    end
   end
 
   def new_feedback(user, subject, content, email, ip)
@@ -75,52 +59,31 @@ class Notifier < ActionMailer::Base
   end
 
   def follow(group, user, followed)
-    domain = group ? group.domain : AppConfig.domain
-    recipients followed.email
-    @language = language_for(followed)
-    set_locale @language
-
-    from from_email(group)
-    subject I18n.t("mailers.notifications.follow.subject", :login => user.login, :app => AppConfig.application_name, :locale => @language)
-    sent_on Time.now
-    body :user => user, :followed => followed, :group => group, :domain => domain
+    setup_email(group, followed) do
+      subject I18n.t("mailers.notifications.follow.subject", :login => user.login, :app => AppConfig.application_name, :locale => @language)
+      body :user => user, :followed => followed, :group => group, :domain => domain_for(group)
+    end
   end
 
   def earned_badge(user, group, badge)
-    domain = group ? group.domain : AppConfig.domain
-    recipients user.email
-    @language = language_for(user)
-    set_locale @language
-
-    from from_email(group)
-    subject I18n.t("mailers.notifications.earned_badge.subject", :group => group.name, :locale => @language)
-    sent_on Time.now
-    body :user => user, :group => group, :badge => badge, :domain => domain
+    setup_email(group, user) do
+      subject I18n.t("mailers.notifications.earned_badge.subject", :group => group.name, :locale => @language)
+      body :user => user, :group => group, :badge => badge, :domain => domain_for(group)
+    end
   end
 
   def favorited(user, group, question)
-    domain = group ? group.domain : AppConfig.domain
-    recipients question.user.email
-    @language = language_for(question.user)
-    set_locale @language
-
-    from from_email(group)
-    subject I18n.t("mailers.notifications.favorited.subject", :login => user.login, :locale => @language)
-    sent_on Time.now
-    body :user => user, :group => group, :question => question, :domain => domain
+    setup_email(group, question.user) do
+      subject I18n.t("mailers.notifications.favorited.subject", :login => user.login, :locale => @language)
+      body :user => user, :group => group, :question => question, :domain => domain_for(group)
+    end
   end
 
   def report(user, report, group)
-    domain = group ? group.domain : AppConfig.domain
-    recipients user.email
-    @language = language_for(user)
-    set_locale @language
-
-    from from_email(group)
-    subject I18n.t("mailers.notifications.report.subject", :group => report.group.name, :app => AppConfig.application_name, :locale => @language)
-    sent_on Time.now
-    content_type    "text/plain"
-    body :user => user, :report => report, :domain => domain
+    setup_email(group, user) do
+      subject I18n.t("mailers.notifications.report.subject", :group => report.group.name, :app => AppConfig.application_name, :locale => @language)
+      body :user => user, :report => report, :domain => domain_for(group)
+    end
   end
 
   private
@@ -128,11 +91,28 @@ class Notifier < ActionMailer::Base
     "#{group ? group.name : AppConfig.application_name} <notifications@#{ActionMailer::Base.default_url_options[:host]}>"
   end
 
+  def domain_for(group)
+    group ? group.domain : AppConfig.domain
+  end
+
+  def setup_email(group, user, &block)
+    @language = language_for(user)
+    set_locale @language
+
+    recipients user.email
+    from from_email(group)
+    sent_on Time.now
+
+    ret = block.call
+
+    ret
+  end
+
   def language_for(user=nil)
-    @language = if user && user.language
-                 @language = user.language
-               else
-                 I18n.locale
-               end
+    if user && user.language
+     user.language
+   else
+     I18n.locale
+   end
   end
 end
