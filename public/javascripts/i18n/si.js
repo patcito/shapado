1d104e8ee86f9148ac9c540304e76ab7edff0f7c
Siebrand Mazeland <s.mazeland@xs4all.nl>
2010-11-29 17:21:26 +0100

    Localisation updates from http://translatewiki.net.

---
+++ public/javascripts/i18n/si.js
@@ -0,0 +1,11 @@
+/** Messages for Sinhala (සිංහල)
+ *  Exported from translatewiki.net
+ *
+ * Translators:
+ *  - Singhalawap
+ */
+
+var I18n = {
+	on_leave_page: "ඔබගේ වෙනස්කිරීම් අහිමිවනු ඇත"
+};
+
