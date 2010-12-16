4b513c0587bf3e03dc0ca50710dd207d2a029f5f
Siebrand Mazeland <s.mazeland@xs4all.nl>
2010-12-16 16:23:15 +0100

    Localisation updates from http://translatewiki.net.

---
+++ public/javascripts/i18n/he.js
@@ -0,0 +1,11 @@
+/** Messages for Hebrew (עברית)
+ *  Exported from translatewiki.net
+ *
+ * Translators:
+ *  - YaronSh
+ */
+
+var I18n = {
+	on_leave_page: "השינויים שלך עלולים ללכת לאיבוד"
+};
+
---
+++ public/javascripts/i18n/shi.js
@@ -0,0 +1,11 @@
+/** Messages for Tachelhit (Tašlḥiyt)
+ *  Exported from translatewiki.net
+ *
+ * Translators:
+ *  - Dalinanir
+ */
+
+var I18n = {
+	on_leave_page: "Radak jlun imbdln lli tskrt ɣ tasna yad"
+};
+
